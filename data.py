db90b1340403ae9d1911a8c359d9ce155e6b0c3a
Genesis Varelli <118129965+GenesisVarelli@users.noreply.github.com>
2024-07-13 13:53:15 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,47 @@
+# Dummy data
+sales_data = {
+    "Product A": 200,
+    "Product B": 400,
+    "Product C": 600,
+    "Product D": 800,
+    "Product E": 1000
+}
+
+inventory_data = {
+    "Product A": 150,
+    "Product B": 75,
+    "Product C": 100,
+    "Product D": 125,
+    "Product E": 150
+}
+
+product_data = {
+    "A": 10,
+    "B": 40,
+    "C": 30,
+    "D": 20,
+    "E": 50
+}
+
+sales_year_data = {
+    2019: 5000,
+    2020: 17500,
+    2021: 10000,
+    2022: 7500,
+    2023: 15000
+}
+
+inventory_month_data = {
+    "Jan": 200,
+    "Feb": 300,
+    "Mar": 800,
+    "Apr": 1300,
+    "May": 600,
+    "Jun": 900,
+    "Jul": 700,
+    "Aug": 900,
+    "Sep": 1000,
+    "Oct": 300,
+    "Nov": 450,
+    "Dec": 1300
+}
